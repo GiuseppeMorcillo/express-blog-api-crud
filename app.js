232f3ce23b9ec576753a5800fbb38eaf9eb42893
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-03-28 14:48:33 +0100

    feat: imposto il tutto per poter usare le rotte e il server

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json()); 
+
+const postsRoutes = require("./routers/router.js"); 
+app.use("/posts", postsRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server in ascolto sulla porta ${PORT}`));
--- routers/router.js
+++ routers/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const postsController = require("../controllers/postsController");
+const postsController = require("../controllers/controllers.js");
 router.get("/", postsController.index);
 router.get("/:id", postsController.show);
 router.delete("/:id", postsController.destroy);
