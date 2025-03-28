6f25b97135623207c7c7b03d6f2156449c7a7f97
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-03-28 14:43:19 +0100

    feat:richiamo le funzioni che ho nel controller e creo le rotte

--- routers/router.js
+++ routers/router.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/postsController");
+router.get("/", postsController.index);
+router.get("/:id", postsController.show);
+router.delete("/:id", postsController.destroy);
+module.exports = router;
