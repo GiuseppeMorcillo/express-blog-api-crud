d42c380340c663ed5f26aad84241f4d34c8b2405
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-03-31 14:39:23 +0200

    feat: imposto la rotta è l'update

--- Crud-2/routers/router.js
+++ Crud-2/routers/router.js
@@ -4,5 +4,6 @@ const postsController = require("../controllers/controllers.js");
 router.get("/", postsController.index);
 router.get("/:id", postsController.show);
 router.post("/store", postsController.store)
+router.put("/update/:id", postsController.update)
 router.delete("/:id", postsController.destroy);
 module.exports = router;
